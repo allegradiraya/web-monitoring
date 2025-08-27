915ee3ec58e1055320d604eab314b90611e1e841
allegradiraya <chillikardianto@gmail.com>
2025-08-27 17:58:49 +0700

    main.tsx

--- team-portal/src/main.tsx
+++ team-portal/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx
 
-createRoot(document.getElementById('root')!).render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+
+import App from './App.tsx';
+import './index.css';
+
+// Gunakan '!' untuk memberitahu TypeScript bahwa elemen 'root' akan selalu ada
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // StrictMode digunakan untuk membantu mendeteksi potensi masalah pada aplikasi
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* BrowserRouter membungkus seluruh aplikasi untuk mengaktifkan routing */}
+    <BrowserRouter>
+      {/* Komponen utama aplikasi */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
